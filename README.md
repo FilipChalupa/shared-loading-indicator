6d2d905ebc5bdb2264213aeb7f8f432fe94a78b5
Filip Chalupa <chalupa.filip@gmail.com>
2022-03-04 17:33:37 +0100

    Add main functionality

--- README.md
+++ README.md
@@ -5,3 +5,98 @@
 ```bash
 npm install shared-loading-indicator
 ```
+
+## How to use
+
+Wrap all by one `SharedLoadingIndicatorContextProvider`.
+
+```jsx
+import { SharedLoadingIndicatorContextProvider } from 'shared-loading-indicator'
+
+export const App => () => {
+	return (
+		<SharedLoadingIndicatorContextProvider>
+			My app
+		</SharedLoadingIndicatorContextProvider>
+	)
+}
+```
+
+### Options
+
+You can optionally configure `startDelay` and `minimalDuration` in milliseconds which will be taken into account in `useSharedLoading` later.
+
+### Hooks
+
+#### useSharedLoading
+
+Hook `useSharedLoading` returns `true` if some component is in loading state. Use this information to show your own loading indicator (spinner, progress bar, …).
+
+```jsx
+import { useSharedLoading } from 'shared-loading-indicator'
+
+export const LoadingIndicator => () => {
+	const isLoading = useSharedLoading()
+
+	if (!isLoading) {
+		return null
+	}
+
+	return (
+		<div>
+			App is loading something
+		</div>
+	)
+}
+```
+
+#### useLocalLoading
+
+Hook `useLocalLoading` works similarly to `useState`. It returns array with `boolean` state indicating that component is loading and set function.
+
+```jsx
+import { useLocalLoading } from 'shared-loading-indicator'
+
+export const MyComponent => () => {
+	const [isLoading, setIsLoading] = useLocalLoading()
+
+	return (
+		<div>
+			<div>
+				Is loading: {isLoading ? 'yes' : 'no'}
+			</div>
+			<button onClick={() => setIsLoading(!isLoading)}>
+		</div>
+	)
+}
+```
+
+```jsx
+import { useLocalLoading } from 'shared-loading-indicator'
+import { useEffect } from 'react'
+
+export const LazyComponent => () => {
+	const [isLoading, setIsLoading] = useLocalLoading()
+	const [data, setData] = useState(null)
+
+	useEffect(() => {
+		setIsLoading(true)
+		fetch('https://example.com')
+			.then(response => response.json())
+			.then(receivedData => {
+				setData(receivedData)
+			})
+	}, [])
+
+	return (
+		<div>
+			<div>
+				Is loading: {isLoading ? 'yes' : 'no'}
+			</div>
+			<pre>
+				{JSON.stringify(data, null, 2)}
+			</pre>
+		</div>
+	)
+}
+```
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,107 @@
-console.log('test')
+import * as React from 'react'
+import {
+	createContext,
+	FunctionComponent,
+	useCallback,
+	useContext,
+	useEffect,
+	useRef,
+	useState,
+} from 'react'
+
+const defaultStartDelay = 300
+const defaultMinimalDuration = 1000
+
+const Context = createContext({
+	count: 0,
+	increment: () => {},
+	decrement: () => {},
+	startDelay: defaultStartDelay,
+	minimalDuration: defaultMinimalDuration,
+})
+
+export const SharedLoadingIndicatorContextProvider: FunctionComponent<{
+	startDelay?: number
+	minimalDuration?: number
+}> = ({
+	children,
+	startDelay = defaultStartDelay,
+	minimalDuration = defaultMinimalDuration,
+}) => {
+	const [count, setCount] = useState(0)
+	const increment = useCallback(() => {
+		setCount((count) => count + 1)
+	}, [])
+	const decrement = useCallback(() => {
+		setCount((count) => count - 1)
+	}, [])
+
+	return (
+		<Context.Provider
+			value={{ count, increment, decrement, startDelay, minimalDuration }}
+		>
+			{children}
+		</Context.Provider>
+	)
+}
+
+export const useLocalLoading = (): [boolean, (isLoading: boolean) => void] => {
+	const [isLoading, setIsLoading] = useState(false)
+	const { increment, decrement } = useContext(Context)
+	const isFirstPass = useRef(true)
+	const isLoadingRef = useRef(false)
+
+	useEffect(() => {
+		if (isFirstPass.current) {
+			isFirstPass.current = false
+		} else {
+			isLoadingRef.current = isLoading
+			if (isLoading) {
+				increment()
+			} else {
+				decrement()
+			}
+		}
+	}, [decrement, increment, isLoading])
+
+	useEffect(() => {
+		return () => {
+			if (isLoadingRef.current) {
+				decrement()
+			}
+		}
+	}, [decrement])
+
+	return [isLoading, setIsLoading]
+}
+
+export const useSharedLoading = () => {
+	const { count, startDelay, minimalDuration } = useContext(Context)
+	const isLoading = count > 0
+	const [isThrottledLoading, setIsThrottledLoading] = useState(isLoading)
+	const loadingStart = useRef(new Date())
+
+	useEffect(() => {
+		if (isLoading) {
+			loadingStart.current = new Date()
+			const timer = setTimeout(() => {
+				setIsThrottledLoading(true)
+			}, startDelay)
+			return () => {
+				clearTimeout(timer)
+			}
+		} else {
+			const now = new Date()
+			const durationAlreadyPassed =
+				now.getTime() - loadingStart.current.getTime() - startDelay
+			const timer = setTimeout(() => {
+				setIsThrottledLoading(false)
+			}, Math.max(0, minimalDuration - durationAlreadyPassed))
+			return () => {
+				clearTimeout(timer)
+			}
+		}
+	}, [isLoading, minimalDuration, startDelay])
+
+	return isThrottledLoading
+}
