35f6ca742085bccbf43fd94922dcf11d161067d0
Filip Chalupa <chalupa.filip@gmail.com>
2023-01-17 14:33:44 +0100

    Clean dist directory on build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import del from 'rollup-plugin-delete'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from 'rollup-plugin-typescript2'
 import packageJson from './package.json'
@@ -19,5 +20,11 @@ export default {
 		},
 	],
 	external: ['react'],
-	plugins: [peerDepsExternal(), resolve(), commonjs(), typescript()],
+	plugins: [
+		del({ targets: path.parse(packageJson.main).dir + '/*' }),
+		peerDepsExternal(),
+		resolve(),
+		commonjs(),
+		typescript(),
+	],
 }
