fe6447ea56d5baa1cb58d1cf7a7225ac93a895e9
Filip Chalupa <chalupa.filip@gmail.com>
2023-01-17 14:35:07 +0100

    Rollup: fix missing dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import path from 'path'
 import del from 'rollup-plugin-delete'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from 'rollup-plugin-typescript2'
