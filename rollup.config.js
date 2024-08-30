7e80a909a3adf7d47101fdbf399642d57e3376f1
Filip Chalupa <chalupa.filip@gmail.com>
2024-08-30 14:21:58 +0200

    Drop commonjs support

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,24 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
 import path from 'path'
 import del from 'rollup-plugin-delete'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import typescript from 'rollup-plugin-typescript2'
 import packageJson from './package.json' assert { type: 'json' }
 
+const outputDirectory = path.parse(packageJson.main).dir
+
 export default {
-	input: './src/index.tsx',
-	output: [
-		{
-			file: packageJson.main,
-			format: 'cjs',
-			sourcemap: true,
-		},
-		{
-			file: packageJson.module,
-			format: 'esm',
-			sourcemap: true,
-		},
-	],
+	input: './src/index.ts',
+	output: {
+		dir: outputDirectory,
+		format: 'esm',
+		sourcemap: true,
+		preserveModules: true,
+	},
 	external: ['react'],
 	plugins: [
-		del({ targets: path.parse(packageJson.main).dir + '/*' }),
+		del({ targets: outputDirectory + '/*' }),
 		peerDepsExternal(),
 		resolve(),
 		commonjs(),
